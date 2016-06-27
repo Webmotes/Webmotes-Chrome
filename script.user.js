afbd6d0a7c28c22247f85072e8322c95c64b2f20
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-27 00:51:00 -0400

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Webmotes
+// @namespace    http://iap-reloaded.github.io/Webmotes
+// @version      0.1
+// @description  emoji to be used over the entire web
+// @author       IAP-Reloaded
+// @match       *
+// @grant        none
+// ==/UserScript==
+
+$(".forum-threadview-post-text:contains('[itworks]')").html(function (_, html) {
+     return html.replace(/[itworks]/g,"<img src='http://i.imgur.com/BjaY6nR.png' />")
+});
+
+$(".forum-threadview-post-text:contains('[itworks-dark]')").html(function (_, html) {
+     return html.replace(/[itworks-dark]/g,"<img src='http://i.imgur.com/2u9WdEk.png' />")
+});
