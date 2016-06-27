9f2d4c718692f60a7dac31c65079a9d6894a5d7b
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-27 07:37:09 -0400

    Added Bill Cipher things and bumped version

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Webmotes
 // @namespace    http://iap-reloaded.github.io/Webmotes
-// @version      0.18
+// @version      0.19
 // @description  emoji to be used over the entire web
 // @author       IAP-Reloaded
 // @match       *://*/*
@@ -17,18 +17,26 @@ $("div:contains('[itworks-dark]')").html(function (_, html) { // dark version of
      return html.replace("[itworks-dark]","<img src='http://i.imgur.com/2u9WdEk.png' />");
 });
 
-$("div:contains('[flipped-smile]')").html(function (_, html) { // dark version of itworks
+$("div:contains('[flipped-smile]')").html(function (_, html) { // flipped smile
      return html.replace("[flipped-smile]","<img src='http://i.imgur.com/OuFD0NS.png' />");
 });
 
-$("div:contains('[frown]')").html(function (_, html) { // dark version of itworks
+$("div:contains('[frown]')").html(function (_, html) { // frown
      return html.replace("[frown]","<img src='http://i.imgur.com/h206D7y.png' />");
 });
 
-$("div:contains('[animationtest]')").html(function (_, html) { // dark version of itworks
+$("div:contains('[animationtest]')").html(function (_, html) { // test for animation
      return html.replace("[animationtest]","<img src='https://i.imgur.com/NhxgpdA.gif' />");
 });
+https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQbFB_48feefIAvILEDvptvUV76sEVRaepT_VbXsXU7etqDcwdT
+$("div:contains('[yourversion]')").html(function (_, html) { // displays the current version
+     return html.replace("[yourversion]","1.19");
+});
 
-$("div:contains('[yourversion]')").html(function (_, html) { // dark version of itworks
-     return html.replace("[yourversion]","1.18");
+$("div:contains('[billcipherwheel]')").html(function (_, html) { // bill cipher wheel
+     return html.replace("[billcipherwheel]","<img src='https://i1.sndcdn.com/artworks-000132224764-hvpveo-small.jpg' />");
+});
+
+$("div:contains('[billcipherdeal]')").html(function (_, html) { // bill cipher making a deal
+     return html.replace("[billcipherdeal]","<img src='https://i1.sndcdn.com/artworks-000058208109-z2pw13-small.jpg' />");
 });
