125f8d3c775801574fe2b1dc47f4e6c9b159f5aa
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-27 01:06:11 -0400

    Trying another way to get jQuery

--- script.user.js
+++ script.user.js
@@ -1,13 +1,23 @@
 // ==UserScript==
 // @name         Webmotes
 // @namespace    http://iap-reloaded.github.io/Webmotes
-// @version      0.6
+// @version      0.7
 // @description  emoji to be used over the entire web
 // @author       IAP-Reloaded
 // @match       http*://reddit.com
 // @grant        none
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
+
+function addJQuery(callback) {
+    var script = document.createElement("script");
+    script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+    script.addEventListener('load', function() {
+        var script = document.createElement("script");
+        script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+        document.body.appendChild(script);
+    }, false);
+    document.body.appendChild(script);
 
 $(".forum-threadview-post-text:contains('[itworks]')").html(function (_, html) {
      return html.replace(/[itworks]/g,"<img src='http://i.imgur.com/BjaY6nR.png' />");
