e1c04551235b52385d542747240956431a7fc1b4
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-07-10 11:19:27 -0400

    1.00! Functions, functionality, Facebook webmote, etc.

--- script.user.js
+++ script.user.js
@@ -13,55 +13,27 @@
 // 0.21: moving this into multiple functions. hope this works.
 // 0.22: Still not working.
 // 0.23: Perhaps removing quotes would work?
+// 1.00: Functions now working! Added facebook one too. Version variable too
 
-function applyWebmote(code, link) {
-    $(div:contains('[' + code + ']')).html(function (_, html) {
-         return html.replace('[' + code + ']',"<img src='" + link + "' />");
-    });
-}
+var version = 1.00
+
+function applyWebmote(code, url) {
+  var replace = "[" + code + "]"
+  $("div:contains(" + replace + ")").html(function(_, html) {
+    return html.replace(replace, "<img src='" + url + "'>");
+  });
+} 
 
 applyWebmote("minecraft-diamond", "https://github.com/IAP-Reloaded/Webmotes/raw/master/Grid_Diamant.png");
-
-$("div:contains('[itworks]')").html(function (_, html) { // proof that it works
-     return html.replace("[itworks]","<img src='http://i.imgur.com/BjaY6nR.png' />");
-});
-
-$("div:contains('[itworks-dark]')").html(function (_, html) { // dark version of itworks
-     return html.replace("[itworks-dark]","<img src='http://i.imgur.com/2u9WdEk.png' />");
-});
-
-$("div:contains('[flipped-smile]')").html(function (_, html) { // flipped smile
-     return html.replace("[flipped-smile]","<img src='http://i.imgur.com/OuFD0NS.png' />");
-});
-
-$("div:contains('[frown]')").html(function (_, html) { // frown
-     return html.replace("[frown]","<img src='http://i.imgur.com/h206D7y.png' />");
-});
-
-$("div:contains('[animationtest]')").html(function (_, html) { // test for animation
-     return html.replace("[animationtest]","<img src='https://i.imgur.com/NhxgpdA.gif' />");
-});
-
-$("div:contains('[yourversion]')").html(function (_, html) { // displays the current version
-     return html.replace("[yourversion]","0.23"); // make sure this stays up to date!
-});
-
-$("div:contains('[billcipherwheel]')").html(function (_, html) { // bill cipher wheel
-     return html.replace("[billcipherwheel]","<img src='https://i1.sndcdn.com/artworks-000132224764-hvpveo-small.jpg' />");
-});
-
-$("div:contains('[billcipherdeal]')").html(function (_, html) { // bill cipher making a deal
-     return html.replace("[billcipherdeal]","<img src='https://i1.sndcdn.com/artworks-000058208109-z2pw13-small.jpg' />");
-});
-
-$("div:contains('[rainbow]')").html(function (_, html) {
-     return html.replace("[rainbow]","<img src='https://raw.githubusercontent.com/IAP-Reloaded/Webmotes/master/Rainbow.png' />");
-});
-
-$("div:contains('[reverse-rainbow]')").html(function (_, html) {
-     return html.replace("[reverse-rainbow]","<img src='https://raw.githubusercontent.com/IAP-Reloaded/Webmotes/master/Backwards%20Rainbow.png' />");
-});
-
-$("div:contains('[itworks-white]')").html(function (_, html) {
-     return html.replace("[itworks-white]","<img src='https://raw.githubusercontent.com/IAP-Reloaded/Webmotes/master/It%20Works%20White.png' />");
-});
+applyWebmote("facebook", "http://ownlocal-adforge-assets.s3.amazonaws.com/production/social/facebook-03b14203ab0e8e3c82388d329d0a57b5d116b7e784060ba214be6571ea6653f8.png");
+applyWebmote("itworks", "http://i.imgur.com/BjaY6nR.png")
+applyWebmote("itworks-dark", "http://i.imgur.com/2u9WdEk.png")
+applyWebmote("flipped-smile", "http://i.imgur.com/OuFD0NS.png")
+applyWebmote("frown", "http://i.imgur.com/h206D7y.png")
+applyWebmote("animationtest", "https://i.imgur.com/NhxgpdA.gif")
+applyWebmote("yourversion", version)
+applyWebmote("billcipherwheel", "https://i1.sndcdn.com/artworks-000132224764-hvpveo-small.jpg")
+applyWebmote("billcipherdeal", "https://i1.sndcdn.com/artworks-000058208109-z2pw13-small.jpg")
+applyWebmote("rainbow", "https://raw.githubusercontent.com/IAP-Reloaded/Webmotes/master/Rainbow.png")
+applyWebmote("reverse-rainbow", "https://raw.githubusercontent.com/IAP-Reloaded/Webmotes/master/Backwards%20Rainbow.png")
+applyWebmote("itworks-white", "https://raw.githubusercontent.com/IAP-Reloaded/Webmotes/master/It%20Works%20White.png")
