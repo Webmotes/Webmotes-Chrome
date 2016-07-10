cdf6de46c680ea4b7aca53a86dc859d80efbe9b3
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-07-10 13:28:11 -0400

    Version numbers are now images

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Webmotes
 // @namespace    http://iap-reloaded.github.io/Webmotes
-// @version      1.03.2
+// @version      1.04
 // @description  emoji to be used over the entire web
 // @author       IAP-Reloaded
 // @match       *://*/*
@@ -17,17 +17,14 @@
 // 1.03: Fixed "==" operator
 // 1.03.1: Fixed var version
 // 1.03.2: Now a string
+// 1.04: Version numbers are now images
 
-var version = "1.03.2"
+var version = "1.04"
 
 function applyWebmote(code, url) {
   var replace = "[" + code + "]"
   $("div:contains(" + replace + ")").html(function(_, html) {
-    if (url == "version") {
       return html.replace(replace, "<img src='" + url + "'>");
-    } else {
-      return html.replace(replace, version);
-    }
   });
 } 
 
@@ -38,7 +35,7 @@ applyWebmote("itworks-dark", "http://i.imgur.com/2u9WdEk.png")
 applyWebmote("flipped-smile", "http://i.imgur.com/OuFD0NS.png")
 applyWebmote("frown", "http://i.imgur.com/h206D7y.png")
 applyWebmote("animationtest", "https://i.imgur.com/NhxgpdA.gif")
-applyWebmote("yourversion", version)
+applyWebmote("yourversion", "https://github.com/IAP-Reloaded/Webmotes/blob/master/1.04%20Version%20Number.png?raw=true") // Make sure to update this!
 applyWebmote("billcipherwheel", "https://i1.sndcdn.com/artworks-000132224764-hvpveo-small.jpg")
 applyWebmote("billcipherdeal", "https://i1.sndcdn.com/artworks-000058208109-z2pw13-small.jpg")
 applyWebmote("rainbow", "https://raw.githubusercontent.com/IAP-Reloaded/Webmotes/master/Rainbow.png")
