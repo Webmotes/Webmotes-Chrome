ed4efeff2ca2587831c46cc9743cb944859dc0a7
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-07-14 18:18:47 -0400

    More version variables, update [version], new [dog]

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Webmotes
 // @namespace    http://iap-reloaded.github.io/Webmotes
-// @version      1.06
+// @version      1.07
 // @description  emoji to be used over the entire web
 // @author       IAP-Reloaded
 // @match        *://*/*
@@ -12,8 +12,11 @@
 // 1.05.1: Linked it directly.
 // 1.05.2: Quick fix for Stack Overflow URL
 // 1.06: Semicolons for the win! Also new Webmotes
+// Some stuff
 
-var version = "1.06";
+var version = "1.07";
+var subversion = "";
+var fullversion = version + subversion;
 
 function applyWebmote(code, image, link) {
   var replace = "[" + code + "]"
@@ -22,6 +25,7 @@ function applyWebmote(code, image, link) {
   });
 } 
 
+applyWebmote("dog", "https://github.com/IAP-Reloaded/Webmotes/blob/master/Peanut%20Sad%20Webmote.png?raw=true", "")
 applyWebmote("googlemaps", "https://showtheway.io/img/showtheway-google-maps.png", "https://maps.google.com");
 applyWebmote("google", "https://addons.cdn.mozilla.net/user-media/addon_icons/613/613484-32.png?modified=1441898710", "https://www.google.com");
 applyWebmote("askubuntu", "http://cdn.sstatic.net/Sites/askubuntu/img/favicon.ico?v=4f32ecc8f43d", "https://www.askubuntu.com");
@@ -33,7 +37,7 @@ applyWebmote("itworks-dark", "http://i.imgur.com/2u9WdEk.png", "");
 applyWebmote("flipped-smile", "http://i.imgur.com/OuFD0NS.png", "");
 applyWebmote("frown", "http://i.imgur.com/h206D7y.png", "");
 applyWebmote("animationtest", "https://i.imgur.com/NhxgpdA.gif", "");
-applyWebmote("yourversion", "https://github.com/IAP-Reloaded/Webmotes/blob/master/1.05%20Version%20Number.png?raw=true", ""); // Make sure to update this!
+applyWebmote("version", "https://github.com/IAP-Reloaded/Webmotes/blob/master/" + version + "%20Version%20Number.png?raw=true", ""); // Make sure to update this! May change version to fullversion later
 applyWebmote("billcipherwheel", "https://i1.sndcdn.com/artworks-000132224764-hvpveo-small.jpg", "");
 applyWebmote("billcipherdeal", "https://i1.sndcdn.com/artworks-000058208109-z2pw13-small.jpg", "");
 applyWebmote("rainbow", "https://raw.githubusercontent.com/IAP-Reloaded/Webmotes/master/Rainbow.png", "");
